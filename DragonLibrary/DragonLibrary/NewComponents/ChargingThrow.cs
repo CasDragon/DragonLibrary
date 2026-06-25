8cf3de1cbf7143645ec9757a894d87db1f195f5d
CasDragon <krb16us@yahoo.com>
2026-06-25 12:59:17 -0400

    update csproj for linux nonsense again Kuru fix to ChargingThrow maybe?

--- DragonLibrary/DragonLibrary/NewComponents/ChargingThrow.cs
+++ DragonLibrary/DragonLibrary/NewComponents/ChargingThrow.cs
@@ -1,7 +1,8 @@
-﻿using JetBrains.Annotations;
+﻿
+using HarmonyLib;
+using JetBrains.Annotations;
 using Kingmaker;
 using Kingmaker.Blueprints;
-using Kingmaker.Blueprints.Classes;
 using Kingmaker.Blueprints.Items.Weapons;
 using Kingmaker.Blueprints.JsonSystem;
 using Kingmaker.Blueprints.Root;
@@ -9,6 +10,8 @@ using Kingmaker.EntitySystem.Entities;
 using Kingmaker.Items;
 using Kingmaker.Localization;
 using Kingmaker.Pathfinding;
+using Kingmaker.TurnBasedMode;
+using Kingmaker.TurnBasedMode.Controllers;
 using Kingmaker.UnitLogic;
 using Kingmaker.UnitLogic.Abilities;
 using Kingmaker.UnitLogic.Abilities.Blueprints;
@@ -17,25 +20,29 @@ using Kingmaker.UnitLogic.Abilities.Components.Base;
 using Kingmaker.UnitLogic.Commands;
 using Kingmaker.Utility;
 using Kingmaker.View;
-using Owlcat.Runtime.Core.Utils;
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using TurnBased.Controllers;
 using UnityEngine;
+using Kingmaker.Visual.Animation.Kingmaker.Actions;
+using Kingmaker.UnitLogic.Commands.Base;
+
 
 namespace DragonLibrary.NewComponents
 {
+    [Serializable]
     [AllowedOn(typeof(BlueprintAbility), false)]
     [TypeId("F6CABA0C-7B39-44D7-BC74-D9CBF0753E6C")]
-    [Serializable]
     public class ChargingThrow : AbilityCustomLogic, IAbilityTargetRestriction, IAbilityMinRangeProvider
     {
-        static readonly LocalizedString FailReasonWeapon = new() { m_Key = "Chargin Hurler Fail Wrong Weapon" };
-        static readonly LocalizedString FailReasonMount = new() { m_Key = "Chargin Hurler Fail Must Run" };
+        private static readonly LocalizedString FailReasonWeapon = new LocalizedString
+        {
+            m_Key = "Charging Hurler Fail Wrong Weapon"
+        };
+
+        private static readonly LocalizedString FailReasonMount = new LocalizedString
+        {
+            m_Key = "Charging Hurler Fail Must Run"
+        };
+
         public override void Cleanup(AbilityExecutionContext context)
         {
             context.Caster.View.AgentASP.IsCharging = false;
@@ -43,145 +50,127 @@ namespace DragonLibrary.NewComponents
             context.Caster.Descriptor.State.IsCharging = false;
         }
 
-        public override IEnumerator<AbilityDeliveryTarget> Deliver(AbilityExecutionContext context, TargetWrapper targetWrapper)
+        public override IEnumerator<AbilityDeliveryTarget> Deliver(AbilityExecutionContext context,
+            TargetWrapper targetWrapper)
         {
-            UnitEntityData target = targetWrapper.Unit;
-            if (target == null)
+            UnitEntityData unit = targetWrapper.Unit;
+            if (unit == null)
             {
-                PFLog.Default.Error("Target unit is missing", Array.Empty<object>());
-                yield break;
+                PFLog.Default.Error("Target unit is missing");
+                return Enumerable.Empty<AbilityDeliveryTarget>().GetEnumerator();
             }
+
             UnitEntityData caster = context.Caster;
-            var position = caster.Position;
-            var endPoint = target.Position;
+            Vector3 position = caster.Position;
+            Vector3 position2 = unit.Position;
             caster.View.StopMoving();
             caster.View.AgentASP.IsCharging = true;
-            caster.View.AgentASP.ForcePath(new ForcedPath(new List<Vector3>
-            {
-                position,
-                endPoint
-            }), true);
-            caster.Descriptor.AddBuff(BlueprintRoot.Instance.SystemMechanics.ChargeBuff, context, new TimeSpan?(1.Rounds().Seconds));
+            //caster.View.AgentASP.ForcePath(new ForcedPath(new List<Vector3> { position, position2 }), disableApproachRadius: true);
+            caster.Descriptor.AddBuff(BlueprintRoot.Instance.SystemMechanics.ChargeBuff, context, 1.Rounds().Seconds);
             caster.Descriptor.State.IsCharging = true;
-            UnitAttack attack = new UnitAttack(target, null);
-            attack.Init(caster);
+            UnitAttack unitAttack = new(unit)
+                { IsCharge = true, MovementType = UnitAnimationActionLocoMotion.WalkSpeedType.Charge };
+            unitAttack.Init(caster);
+            var routine = (CombatController.IsInTurnBasedCombat()
+                ? TurnBasedRoutine(caster, unit, unitAttack)
+                : RuntimeRoutine(caster, unit, unitAttack, position2));
+            return routine;
+        }
 
-            IEnumerator routine = CombatController.IsInTurnBasedCombat() ? TurnBasesRoutine(caster, target, attack) : RuntimeRoutine(caster, target, attack, endPoint);
-            while (routine.MoveNext())
-                yield return null;
-        }
-        private static IEnumerator TurnBasesRoutine(UnitEntityData caster, UnitEntityData target, UnitAttack attack)
-        {            
-            UnitMovementAgent agentASP = caster.View.AgentASP;
-            float timeSinceStart = 0f;
-            float passedDistance = 0f;
-            var distance = (caster.Position - target.Position).magnitude;
-            while (distance > 30f || (attack.ShouldUnitApproach && passedDistance < 5))
-            {
-                passedDistance += (caster.Position - caster.PreviousPosition).magnitude;
-                if (Game.Instance.TurnBasedCombatController.WaitingForUI)
-                {
-                    yield return null;
-                }
-                else
-                {
-                    timeSinceStart += Game.Instance.TimeController.GameDeltaTime;
-                    if (timeSinceStart > 6f)
-                    {
-                        break;
-                    }
-                    if (!caster.Descriptor.State.CanMove)
-                    {
-                        break;
-                    }
-                    if (!agentASP)
-                    {
-                        break;
-                    }
-                    if (!agentASP.IsReallyMoving)
-                    {
-                        agentASP.ForcePath(new ForcedPath(new List<Vector3>
-                        {
-                            caster.Position,
-                            target.Position
-                        }), true);
-                        if (!agentASP.IsReallyMoving)
-                        {
-                            break;
-                        }
-                    }
-                    agentASP.MaxSpeedOverride = new float?(Math.Max(agentASP.MaxSpeedOverride.GetValueOrDefault(), caster.CombatSpeedMps * 2f));
-                    yield return null;
-                }
-            }
-            caster.View.StopMoving();
-            if (!attack.ShouldUnitApproach)
-            {
-                attack.IgnoreCooldown(null);
-                attack.IsCharge = true;
-            }
-            caster.Commands.AddToQueueFirst(attack);
-            yield break;
-        }
-        private static IEnumerator RuntimeRoutine(UnitEntityData caster, UnitEntityData target, UnitAttack attack, Vector3 endPoint)
+        private static IEnumerator<AbilityDeliveryTarget> TurnBasedRoutine(UnitEntityData caster, UnitEntityData _,
+            UnitAttack attack)
         {
-            float maxDistance = GetMaxRangeMeters(caster)-30f;
+            caster.View.AgentASP.MaxSpeedOverride = Math.Max(caster.View.AgentASP.MaxSpeedOverride.GetValueOrDefault(),
+                caster.CombatSpeedMps * 2f);
+            var currenTurn = Game.Instance.TurnBasedCombatController.CurrentTurn;
+            currenTurn.CurrentMovementLimit = TurnController.MovementLimit.OneAction;
+            var colldown = currenTurn.GetCooldown(caster);
+            colldown.MoveAction = 0;
+            colldown.StandardAction = 0;
+            caster.Commands.Run(attack);
+            while (attack.Result is not UnitCommand.ResultType.Success or UnitCommand.ResultType.Interrupt)
+                yield return null;
+            colldown.MoveAction = 6;
+            caster.Descriptor.State.IsCharging = false;
+        }
+
+        private static IEnumerator<AbilityDeliveryTarget> RuntimeRoutine(UnitEntityData caster, UnitEntityData target,
+            UnitAttack attack, Vector3 endPoint)
+        {
+            float maxDistance = GetMaxRangeMeters(caster) - 30f;
             float passedDistance = 0f;
             while (caster.View.MovementAgent.IsReallyMoving)
             {
-                caster.View.MovementAgent.MaxSpeedOverride = new float?(Math.Max(caster.View.MovementAgent.MaxSpeedOverride.GetValueOrDefault(), caster.CombatSpeedMps * 2f));
+                caster.View.MovementAgent.MaxSpeedOverride = Math.Max(
+                    caster.View.MovementAgent.MaxSpeedOverride.GetValueOrDefault(), caster.CombatSpeedMps * 2f);
                 passedDistance += (caster.Position - caster.PreviousPosition).magnitude;
-                if (passedDistance > maxDistance || (passedDistance > 5 && !attack.ShouldUnitApproach))
+                if (passedDistance > maxDistance || (passedDistance > 5f && !attack.ShouldUnitApproach))
                 {
                     break;
                 }
+
                 Vector3 position = target.Position;
                 if (ObstacleAnalyzer.TraceAlongNavmesh(caster.Position, position) != position)
                 {
                     break;
                 }
+
                 if (position != endPoint)
                 {
                     endPoint = position;
-                    caster.View.AgentASP.ForcePath(new ForcedPath(new List<Vector3>
-                    {
-                        caster.Position,
-                        endPoint
-                    }), true);
+                    caster.View.AgentASP.ForcePath(new ForcedPath(new List<Vector3> { caster.Position, endPoint }),
+                        disableApproachRadius: true);
                 }
+
                 yield return null;
-            }    
+            }
+
             if (!attack.ShouldUnitApproach)
             {
-                attack.IgnoreCooldown(null);
+                attack.IgnoreCooldown();
                 attack.IsCharge = true;
             }
+
             caster.Commands.AddToQueueFirst(attack);
-            yield break;
         }
+
         public string GetAbilityTargetRestrictionUIText(UnitEntityData caster, TargetWrapper target)
         {
-            CheckTargetRestriction(caster, target, out var localizedString);
-            return localizedString ?? string.Empty;
+            CheckTargetRestriction(caster, target, out var failReason);
+            LocalizedString localizedString = failReason;
+            if (localizedString == null)
+            {
+                return string.Empty;
+            }
+
+            return localizedString;
         }
 
         public float GetMinRangeMeters(UnitEntityData caster)
         {
             return AbilityCustomCharge.GetMinRangeMeters(caster, null);
         }
+
         public static float GetMinRangeMeters(UnitEntityData caster, [CanBeNull] UnitEntityData target)
         {
             float num = target?.View.Corpulence ?? 0.5f;
             if (Game.Instance.Player.IsTurnBasedModeOn())
-                return TurnController.MetersOfFiveFootStep *2 + GameConsts.MinWeaponRange.Meters + caster.View.Corpulence + num;            
-            else
-                return 15.Feet().Meters + caster.View.Corpulence + num;
+            {
+                return TurnController.MetersOfFiveFootStep * 2f + GameConsts.MinWeaponRange.Meters +
+                       caster.View.Corpulence + num;
+            }
+
+            return 15.Feet().Meters + caster.View.Corpulence + num;
         }
+
         public bool IsTargetRestrictionPassed(UnitEntityData caster, TargetWrapper targetWrapper)
         {
-            return CheckTargetRestriction(caster, targetWrapper, out var localizedString);
+            LocalizedString failReason;
+            return CheckTargetRestriction(caster, targetWrapper, out failReason);
         }
-        private bool CheckTargetRestriction(UnitEntityData caster, TargetWrapper targetWrapper, [CanBeNull] out LocalizedString failReason)
+
+        private bool CheckTargetRestriction(UnitEntityData caster, TargetWrapper targetWrapper,
+            [CanBeNull] out LocalizedString failReason)
         {
             UnitEntityData unitEntityData = targetWrapper?.Unit;
             if (unitEntityData == null)
@@ -189,39 +178,140 @@ namespace DragonLibrary.NewComponents
                 failReason = BlueprintRoot.Instance.LocalizedTexts.Reasons.TargetIsInvalid;
                 return false;
             }
-            if (!(caster.Body.PrimaryHand.MaybeWeapon?.Blueprint.Type.FighterGroup.Contains(WeaponFighterGroup.Thrown)?? false))
+
+            ItemEntityWeapon maybeWeapon = caster.Body.PrimaryHand.MaybeWeapon;
+            if (maybeWeapon == null || !maybeWeapon.Blueprint.Type.FighterGroup.Contains(WeaponFighterGroup.Thrown))
             {
                 failReason = FailReasonWeapon;
                 return false;
             }
+
             if (caster.GetSaddledUnit() != null)
             {
                 failReason = FailReasonMount;
                 return false;
             }
+
             float magnitude = (unitEntityData.Position - caster.Position).magnitude;
             if (magnitude > GetMaxRangeMeters(caster))
             {
                 failReason = BlueprintRoot.Instance.LocalizedTexts.Reasons.TargetIsTooFar;
                 return false;
             }
+
             if (magnitude < GetMinRangeMeters(caster, unitEntityData))
             {
                 failReason = BlueprintRoot.Instance.LocalizedTexts.Reasons.TargetIsTooClose;
                 return false;
             }
+
             if (ObstacleAnalyzer.TraceAlongNavmesh(caster.Position, unitEntityData.Position) != unitEntityData.Position)
             {
                 failReason = BlueprintRoot.Instance.LocalizedTexts.Reasons.ObstacleBetweenCasterAndTarget;
                 return false;
             }
+
             failReason = null;
             return true;
         }
+
         public static float GetMaxRangeMeters(UnitEntityData caster)
         {
             return caster.CombatSpeedMps * 6f + 30f;
         }
+    }
 
+    [UsedImplicitly]
+    [HarmonyPatch]
+    static class ChargingThrowPatches
+    {
+
+        [UsedImplicitly]
+        [HarmonyPatch(typeof(PathVisualizer), nameof(PathVisualizer.UpdateChargePath))]
+        [HarmonyPostfix]
+        static void PathVisualizerPreviewPostfix(ref bool __result, PathVisualizer __instance)
+        {
+            if (__result)
+                return;
+
+            AbilityData abilityData = Game.Instance.SelectedAbilityHandler?.SelectedAbility;
+            if (abilityData == null)
+            {
+                return;
+            }
+
+            if (abilityData.Blueprint.GetComponent<ChargingThrow>() is null)
+            {
+                return;
+            }
+
+            GameObject gameObject = Game.Instance.DefaultPointerController.PointerOn;
+            if (!gameObject)
+            {
+                return;
+            }
+
+            var view = gameObject.GetComponent<UnitEntityView>();
+            if (!view)
+            {
+                return;
+            }
+
+            var targetUnitData = view.Data;
+            var caster = abilityData.Caster.Unit;
+            var startingPosition = caster.Position;
+            var direction = (targetUnitData.Position - startingPosition).normalized;
+            var targetPosition = targetUnitData.Position - (direction *
+                                                            (UnitAttack.GetApproachRadius(caster.GetFirstWeapon(),
+                                                                caster, targetUnitData) - 0.1f));
+            TargetWrapper targetWrapper = new(targetPosition, null, targetUnitData);
+            if (!abilityData.CanTarget(targetWrapper))
+            {
+                return;
+            }
+
+
+            if (__instance.m_CurrentPath != null)
+            {
+                if (__instance.m_CurrentPath is ForcedPath { UserTag: "ChargePathTag" } &&
+                    __instance.m_CurrentPath.vectorPath.Count == 2 &&
+                    (__instance.m_CurrentPath.vectorPath[0] - startingPosition).sqrMagnitude < 0.1f &&
+                    (__instance.m_CurrentPath.vectorPath[1] - targetPosition).sqrMagnitude < 0.1f)
+                {
+                    __result = true;
+                    return;
+                }
+            }
+
+            var forcedPath2 = new ForcedPath([startingPosition, targetPosition])
+            {
+                UserTag = "ChargePathTag"
+            };
+            __instance.m_RequestedPath = null;
+            __instance.m_CurrentPath = forcedPath2;
+            __instance.m_CurrentPath.Claim(__instance);
+            var MaxDistance = caster.CombatSpeedMps * 6f;
+            PathVisualizer.VisualPathSettings visualPathSettings = new PathVisualizer.VisualPathSettings
+            {
+                Unit = caster,
+                UnitCanGetUpOnCommand = false,
+                MoveActionCooldown = 0f,
+                MovePredictionState = CombatAction.ActionState.Available,
+                StandardPredictionState = CombatAction.ActionState.Available,
+                RemainingMovementRange = MaxDistance,
+                RemainingMovementRangeTotal = MaxDistance,
+                EnabledSingleActionMove = true,
+                EnabledFiveFeetStep = false
+            };
+            __instance.UpdateVisualPath(targetPosition, 0.3f, true, -1f, visualPathSettings);
+            __result = true;
+
+        }
+
+        [UsedImplicitly]
+        [HarmonyPatch(typeof(UnitEntityData), nameof(UnitEntityData.IsMoveActionRestricted))]
+        [HarmonyPostfix]
+        static bool UnitEntityDataMoveActionRstrictedPostfix(bool __result, UnitEntityData __instance)
+            => __result && !__instance.Descriptor.State.IsCharging;
     }
 }
