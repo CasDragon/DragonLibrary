faff6c0e38da6dc64f13c39e764535b17b01222c
CasDragon <krb16us@yahoo.com>
2026-08-17 12:44:25 -0400

    z

--- DragonLibrary/DragonLibrary/Utils/DragonHelpers.cs
+++ DragonLibrary/DragonLibrary/Utils/DragonHelpers.cs
@@ -21,5 +21,11 @@ namespace DragonLibrary.Utils
             spellList.SpellsByLevel[spellLevel].m_Spells = spellList.SpellsByLevel[spellLevel].m_Spells.Except([spellReference]).ToList();
             spellList.SpellsByLevel[spellLevel].m_SpellsProxy?.Remove(spell);
         }
+        public static T MakeRef<T>(string guid) 
+            where T : BlueprintReferenceBase?, new()
+        {
+            var x = ResourcesLibrary.TryGetBlueprint(new BlueprintGuid(new Guid(guid)));
+            return x.ToReference<T>();
+        }
     }
 }
